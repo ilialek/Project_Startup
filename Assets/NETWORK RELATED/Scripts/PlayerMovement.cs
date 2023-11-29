dc630705416ef9a4ceb09cb2eac474df614bf4d6
ilialek <iklekomcev200413@gmail.com>
2023-11-29 16:33:20 +0100

    Some basic Network additions
    Some basic network functionality added to the SampleScene

---
+++ Assets/NETWORK RELATED/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    Rigidbody rb;
+
+    [Header("Ground movement and jump settings")]
+    public float speed = 12f;
+    public float groundDrag;
+    public float airFriction;
+    public float jumpForce;
+    public float jumpHeightCheck;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+
+    void Update()
+    {
+        if (!IsOwner) { return; }
+
+        JumpAndDrag();
+        SpeedControl();
+    }
+
+    void FixedUpdate()
+    {
+        if (!IsOwner) { return; }
+        GroundMovement();
+    }
+
+
+
+    void GroundMovement()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (isGrounded)
+        {
+            rb.AddForce(move.normalized * speed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(move.normalized * speed * 10f * airFriction, ForceMode.Force);
+        }
+
+    }
+
+    void SpeedControl()
+    {
+        Vector3 XZVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if (XZVelocity.magnitude > speed)
+        {
+            Vector3 normalizedVelocity = XZVelocity.normalized * speed;
+            rb.velocity = new Vector3(normalizedVelocity.x, rb.velocity.y, normalizedVelocity.z);
+        }
+    }
+
+    void JumpAndDrag()
+    {
+
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, jumpHeightCheck, whatIsGround);
+
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+}
