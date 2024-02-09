798db5256da28fe09171f5d02a914d761e6608e1
dumiswa <dumitrudumi605@yahoo.com>
2024-02-09 01:35:42 +0100

    mainMenuSounds

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioSource clickButtonSound;
+    public AudioSource hoverOverButtonSound;
 
     public void PlayClickButtonSound()
     {
         clickButtonSound.Play();
+    }
+
+    public void PlayHoverOverButtonSound()
+    {
+        if(hoverOverButtonSound.clip != null && !hoverOverButtonSound.isPlaying)
+        {
+            hoverOverButtonSound.Play();
+        }     
     }
 }
